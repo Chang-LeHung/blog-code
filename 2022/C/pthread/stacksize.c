bcb087e5745b171faf42c0fc5bda9671b35f8b3f
Chang-LeHung <799207539@qq.com>
2022-10-30 20:03:07 +0800

    update pthread

---
+++ 2022/C/pthread/stacksize.c
@@ -0,0 +1,30 @@
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int times = 1;
+u_int64_t rbp;
+
+void* func(void* arg) {
+  asm volatile(
+    "movq %%rbp, %0;"
+    :"=m"(rbp)::
+  );
+  printf("times = %d rbp = %lx\n", times, rbp);
+  times++;
+  char s[1 << 20];
+  func(NULL);
+  return NULL;
+}
+
+int main() {
+
+  pthread_t t;
+  pthread_create(&t, NULL, func, NULL);
+  pthread_join(t, NULL);
+  // func(NULL);
+  return 0;
+}
