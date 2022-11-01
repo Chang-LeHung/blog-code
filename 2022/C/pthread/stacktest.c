0be7ddad2814772f36cbbe1c778c30c810137e15
Chang-LeHung <799207539@qq.com>
2022-11-01 21:54:19 +0800

    add more instroduction demos

--- 2022/C/pthread/stacktest.c
+++ 2022/C/pthread/stacktest.c
@@ -5,28 +5,18 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 int times = 1;
-u_int64_t rbp;
 
 void* func(void* arg) {
-  #if __X86__
-      asm volatile(
-      "movq %%rbp, %0;"
-      :"=m"(rbp)::
-    );
-    printf("times = %d rbp = %lx\n", times, rbp);
-  #else
-    printf("times = %d\n", times);
-  #endif
+  printf("times = %d\n", times);
   times++;
-  char s[1 << 20];
+  char s[1 << 20]; // 申请 1MB 内存空间（分配在栈空间上）
   func(NULL);
   return NULL;
 }
 
 int main() {
-  setbuf(stdout, NULL);
+
   pthread_t t;
   pthread_create(&t, NULL, func, NULL);
   pthread_join(t, NULL);
