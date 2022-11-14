f2510d5edcd14ec91c3b016b37ca5cebb5c2b0da
Chang-LeHung <799207539@qq.com>
2022-11-14 20:04:51 +0800

    update pthread tutorial02

--- 2022/C/pthread/cancel01.c
+++ 2022/C/pthread/cancel01.c
@@ -9,9 +9,10 @@
 
 void* task(void* arg) {
   pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
+  sleep(1);
   int a = 1;
   while(1) {
-    a++;
+    
   }
   return NULL;
 }
@@ -21,10 +22,10 @@ int main() {
   void* res;
   pthread_t t1;
   pthread_create(&t1, NULL, task, NULL);
+  printf("Hello World\n");
   int s = pthread_cancel(t1);
   if(s != 0) // s == 0 mean call successfully
     fprintf(stderr, "cancel failed\n");
-  printf("Hello World\n");
   pthread_join(t1, &res);
   assert(res == PTHREAD_CANCELED);
   return 0;
