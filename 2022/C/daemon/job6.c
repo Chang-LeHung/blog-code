8e5f469714cbf9e04b6863b4efaeb82908c3671c
Chang-LeHung <changlehung@qq.com>
2022-11-30 05:08:16 +0000

    job control

--- 2022/C/daemon/job6.c
+++ 2022/C/daemon/job6.c
@@ -9,12 +9,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <assert.h>
 
 void my_handler (int signo, siginfo_t *si, void*ucontext)
 {
   char s[1024];
+  int fd = open("text.txt", O_APPEND | O_RDWR | O_CREAT, 0644);
+  if(fd == -1)
+  {
+    perror("");
+    abort();
+  }
   sprintf(s, "发送信号的进程号 = %d 信号 = %d\n", si->si_pid, signo);
-  write(STDOUT_FILENO, s, strlen(s));
+  write(fd, s, strlen(s));
+  close(fd);
+  fsync(fd);
   _exit(0);
 }
 
@@ -24,9 +33,11 @@ int main()
   struct sigaction demo;
   demo.sa_sigaction = my_handler;
   demo.sa_flags |= SA_SIGINFO;
+  demo.sa_flags &= ~SA_RESETHAND;
   sigaction(SIGINT, &demo, NULL);
   sigaction(SIGQUIT, &demo, NULL);
   sigaction(SIGTSTP, &demo, NULL);
+  sigaction(SIGHUP, &demo, NULL);
   while(1);
   return 0;
 }
