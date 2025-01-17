c05d55097d7f8389ea256b1b9ef09c2b1a6cd526
changlehung <changlehung@gmail.com>
2025-01-17 21:18:34 +0800

    feat: cat initial commit

--- 2025/cat/cat.go
+++ 2025/cat/cat.go
@@ -1,7 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	EXIT_FAILURE = 1
+)
+
+type CommandLine struct {
+	Args    []string
+	LineNum bool
+	Files   []string
+}
+
+func Fatal(msg string) {
+	_, _ = fmt.Fprintf(os.Stderr, "%s", msg)
+	os.Exit(EXIT_FAILURE)
+}
+
+func NewCommandLine(args []string) *CommandLine {
+	return &CommandLine{
+		Args: args,
+	}
+}
+
+func (c *CommandLine) Parse() {
+	for _, arg := range c.Args {
+		if arg == "-n" {
+			c.LineNum = true
+		} else {
+			c.Files = append(c.Files, arg)
+		}
+	}
+}
+
+func (c *CommandLine) ContaineArgs(s string) bool {
+	for _, v := range c.Args {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *CommandLine) Print() {
+	for _, file := range c.Files {
+		fi, err := os.Stat(file)
+		if err != nil {
+			Fatal(err.Error())
+		}
+		if fi.Mode().IsRegular() {
+			no := 1
+			f, err := os.Open(file)
+			if err != nil {
+				Fatal(err.Error())
+
+			}
+			defer f.Close()
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				line := scanner.Text()
+				if c.LineNum {
+					fmt.Printf("%d\t%s\n", no, line)
+					no++
+				} else {
+					fmt.Println(line)
+				}
+			}
+		} else {
+			Fatal(fmt.Sprintf("%s is not a regular file", fi.Name()))
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	c := NewCommandLine(os.Args[1:])
+	if c.ContaineArgs("-h") || c.ContaineArgs("--help") {
+		fmt.Println("Usage: cat [OPTION]... [FILE]...")
+		os.Exit(0)
+	}
+	c.Parse()
+	c.Print()
 }
